cf6b2d45ef858ec01806b3562b3e8cb7fe8d350c
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-12 22:07:44 -0400

    Added 'show' route, method, and view for 'Goal'.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,13 @@
 class GoalsController < ApplicationController
   def index
-      @goals = Goal.all
+    @goals = Goal.all
+  end
+
+  def show
+    begin
+      @goal = Goal.find( params[ :id ] )
+    rescue
+      @goal = nil
+    end
   end
 end
