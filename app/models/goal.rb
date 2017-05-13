a08bd31a468af97f0106c4338343882ca2d8fb85
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-12 21:53:59 -0400

    Added validates for 'Goal' name and description properties. Built out initial tests for 'Goal'.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,4 @@
 class Goal < ActiveRecord::Base
+    validates :name, :presence => true
+    validates :description, :presence => true
 end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -4,4 +4,23 @@ class GoalTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  # ENSURE THAT 'name' PROP EXISTS
+  test "should not save Goal without name" do
+      goal = Goal.new( { :description => "Test description" } )
+
+      assert_not goal.save
+  end
+  
+  test "should not save Goal without description" do
+      goal = Goal.new( { :name => "Test Goal" } )
+      
+      assert_not goal.save
+  end
+  
+  test "should save Goal when name and description are present" do
+      goal = Goal.new( { :name => "Test Goal", :description => "Test description" } )
+      
+      assert goal.save
+  end
 end
