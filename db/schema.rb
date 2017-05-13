08974c866e5aa5b751b2757430f4e7e07ac2c708
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-12 22:27:13 -0400

    Added 'goal_id' property to 'Task'.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513021129) do
+ActiveRecord::Schema.define(version: 20170513021536) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170513021129) do
     t.date     "completed_on"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "goal_id"
   end
 
 end
